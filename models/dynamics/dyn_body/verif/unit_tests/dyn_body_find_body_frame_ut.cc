96fc6c92d6f83d27516db40a82684145f2501d13
Sylvester Kaczmarek <16242628+sylvesterkaczmarek@users.noreply.github.com>
2026-09-09 01:30:40 +0100

    Guard DynBody frame lookup before initialization

--- models/dynamics/dyn_body/verif/unit_tests/dyn_body_find_body_frame_ut.cc
+++ models/dynamics/dyn_body/verif/unit_tests/dyn_body_find_body_frame_ut.cc
@@ -3,10 +3,23 @@
  */
 
 #include "dynamics/dyn_body/include/dyn_body.hh"
+#include "dynamics/dyn_body/include/dyn_body_messages.hh"
+#include "message_handler_mock.hh"
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
 using namespace jeod;
+using testing::_;
 
-TEST(DynBody, find_body_frame) {}
+TEST(DynBody, find_body_frame_without_dyn_manager)
+{
+    MockMessageHandler mockMessageHandler;
+    DynBody body;
+
+    EXPECT_CALL(mockMessageHandler,
+                process_message(MessageHandler::Failure, _, _, _, DynBodyMessages::invalid_body, _, _))
+        .Times(1);
+
+    EXPECT_EQ(nullptr, body.find_body_frame("structure"));
+}
